bbbe01cfcd1c5adbc3e315e6d9120f693de9297f
‘eastsheng <eastsheng@hotmail.com>
2023-09-13 00:34:16 +0800

    upload

---
+++ MolCalculator/MolCalculator.bat
@@ -0,0 +1,1 @@
+python MolCalculator.py
---
+++ MolCalculator/build_shortcut.bat
@@ -0,0 +1,30 @@
+@echo off
+
+set Program=%cd%\MolCalculator.bat
+   
+set LnkName=MolCalculator
+ 
+set WorkDir=%cd%
+ 
+set Desc=MolCalculator
+set icon=%cd%/imgs/icons-64.ico
+
+if not defined WorkDir call:GetWorkDir "%Program%"
+(echo Set WshShell=CreateObject("WScript.Shell"^)
+echo strDesKtop=WshShell.SpecialFolders("DesKtop"^)
+echo Set oShellLink=WshShell.CreateShortcut(strDesKtop^&"\%LnkName%.lnk"^)
+echo oShellLink.TargetPath="%Program%"
+echo oShellLink.WorkingDirectory="%WorkDir%"
+echo oShellLink.WindowStyle=1
+echo oShellLink.Description="%Desc%"
+echo oShellLink.IconLocation="%icon%"
+echo oShellLink.Save)>makelnk.vbs
+echo Successfully！ 
+makelnk.vbs
+del /f /q makelnk.vbs
+exit
+goto :eof
+:GetWorkDir
+set WorkDir=%~dp1
+set WorkDir=%WorkDir:~,-1%
+goto :eof
